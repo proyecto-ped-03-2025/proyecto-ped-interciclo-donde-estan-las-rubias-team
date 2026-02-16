2f2f856c9e86d287c1f5d7e395fc35154bbf7e83
Bran-ssj <00109425@uca.edu.sv>
2026-02-15 18:46:46 -0600

    Add: funcion para validaciones

--- inscribir.h
+++ inscribir.h
@@ -1,32 +1,74 @@
 #ifndef INSCRIBIR_H
 #define INSCRIBIR_H
 
-void Juego::insertar(){
-    Jugador* nuevo;
+void Juego::insertar()
+{
+    Jugador *nuevo;
     char respuesta;
 
-    do {
+    do
+    {
         cout << "¿Quieres inscribir un jugador nuevo? (s/n): ";
         cin >> respuesta;
 
-        if(respuesta == 's'){
+        if (respuesta == 's')
+        {
             nuevo = new Jugador;
             nuevo->puntaje = 0;
 
+            bool correcto;
+            do
+            {
+                correcto = true;
+                cout << "Nombre: ";
+                cin >> nuevo->nombre;
 
-            cout << "Nombre: ";
-            cin >> nuevo->nombre;
+                for (int i = 0; i < nuevo->nombre.length(); i++)
+                {
+                    if (isdigit(nuevo->nombre[i]))
+                    {
+                        correcto = false;
+                    }
+                }
 
-            cout << "Identificador numerico: ";
-            cin >> nuevo->identificador;
+                if (!correcto)
+                    cout << "Solo se permiten letras.\n";
 
-            if(pInicio == NULL){
+            } while (!correcto);
+
+            string temp;
+           
+
+            do
+            {
+                correcto = true;
+                cout << "Identificador numerico: ";
+                cin >> temp;
+
+                for (int i = 0; i < temp.length(); i++)
+                {
+                    if (!isdigit(temp[i]))
+                    {
+                        correcto = false;
+                    }
+                }
+
+                if (!correcto)
+                    cout << "Solo se permiten numeros.\n";
+
+            } while (!correcto);
+
+            nuevo->identificador = stoi(temp);
+
+            if (pInicio == NULL)
+            {
                 nuevo->sig = nuevo;
                 nuevo->ant = nuevo;
                 pInicio = nuevo;
             }
-            else{
-                Jugador* ultimo = pInicio->ant;
+            else
+            {
+                Jugador *ultimo = pInicio->ant;
 
                 nuevo->sig = pInicio;
                 nuevo->ant = ultimo;
